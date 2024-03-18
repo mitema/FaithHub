18c4e697adfce06a32f3fd8069d36214994a7a31
Mitema <mitema.emmanuel@gmail.com>
2024-03-18 11:28:00 +0100

    removed jwt to master

--- helper/generateKey.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-
-//generate 2 secrets: one for access and refresh tokens 
-const getSecret = () =>  crypto.randomBytes(32).toString('hex')
-
-
-module.exports = {
-    getSecret
-}
