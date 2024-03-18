fb80c1936c0b7b4e7195bcc2918294233cce4c28
WoleIlori <gideonilori@yahoo.com>
2024-03-18 14:15:40 +0000

    modify comment

--- helper/generateKey.js
+++ helper/generateKey.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
 
-//generate 2 secrets: one for access and refresh tokens 
+//generate secrets: one for access and refresh tokens 
 const getSecret = () =>  crypto.randomBytes(32).toString('hex')
 
 
